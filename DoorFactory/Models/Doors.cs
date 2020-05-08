3e5e2f864a790f2ab438dc87a4474532a5abbd68
Roman <brych.126@gmail.com>
2020-05-08 23:27:16 +0300

    Finish with form and validation

--- DoorFactory/Models/Doors.cs
+++ DoorFactory/Models/Doors.cs
@@ -14,10 +14,13 @@ namespace DoorFactory.Models
 
         public int DoorId { get; set; }
         [Required(ErrorMessage = "Поле Ширина є обовязковим")]
+        [Range(500, 5000,ErrorMessage = "Ширина дверей повинна бути в межах від 0.5 до 5 метрів.")]
         public int Width { get; set; }
         [Required(ErrorMessage = "Поле Висота є обовязковим")]
+        [Range(1500, 3000, ErrorMessage = "Висота дверей повинна бути в межах від 1.5 до 3 метрів.")]
         public int Height { get; set; }
         [Required(ErrorMessage = "Поле Товщина є обовязковим")]
+        [Range(30, 100, ErrorMessage = "Товщина дверей повинна бути в межах від 3 до 10 сантиметрів.")]
         public int Thickness { get; set; }
         public decimal Price { get; set; }
         [Required(ErrorMessage = "Поле Категорія є обовязковим")]
@@ -27,11 +30,12 @@ namespace DoorFactory.Models
         [Required(ErrorMessage = "Поле Стиль є обовязковим")]
         public int StyleTypesId { get; set; }
         public string DoorName { get; set; }
+        [MinLength(5, ErrorMessage = "Текст повинен містити щонайменше 5 символів")]
+        [MaxLength(255,ErrorMessage = "Текст не повинен перевищувати 255 символів")]
         public string Description { get; set; }
         [Required(ErrorMessage = "Поле Колір є обовязковим")]
         public int ColorId { get; set; }
         public double Rate { get; set; }
-
         public virtual Colors Color { get; set; }
         public virtual DoorCategories DoorsCategories { get; set; }
         public virtual OpeningStyles OpeningStyles { get; set; }
